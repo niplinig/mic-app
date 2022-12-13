b0da23e71ac063ffb30cd13f220efd7233f414ab
Nicolás Plaza <niplinig@espol.edu.ec>
2022-12-13 11:07:24 -0500

    Dinamic change

--- pages/videos/index.tsx
+++ pages/videos/index.tsx
@@ -1,14 +1,14 @@
-import { Button, Note, Spacer, Tabs, Input, Radio, Text, Pagination, Card, Grid, Checkbox, useInput, useTabs, useRect, useKeyboard, useToasts } from "@geist-ui/core";
+import { Button, Display, Note, Spacer, Tabs, Input, Radio, Text, Pagination, Card, Grid, Checkbox, useInput, useTabs, useRect, useKeyboard, useToasts } from "@geist-ui/core";
 import { useEffect, useRef, useState } from "react";
 import { KeyCode } from "@geist-ui/core";
 
 let videoStart: string = '';
 let videoEnd: string = '';
-let reactionTime : string = '';
+let reactionTime: string = '';
 let reactionStart: string = '';
 
 export default function App() {
-    
+
     const video: any = useRef<HTMLVideoElement>(null);
 
     const { state, setState, bindings } = useTabs('1');
@@ -32,7 +32,7 @@ export default function App() {
                 reactionTime = `${video.current.currentTime}`;
                 reactionStart = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}:${date.getMilliseconds()}`
                 console.log(`reactionTime ${reactionTime}, reactionStart: ${reactionStart}`);
-                setToast({text: 'Reacción guardada', delay: 2000});
+                setToast({ text: 'Reacción guardada', delay: 2000 });
             }
         },
         [KeyCode.KEY_R],
@@ -48,7 +48,9 @@ export default function App() {
     }
 
     const changePage = () => {
-        setState('3');
+        let previus = state;
+        let result : string = `${Number(previus) + 1}`
+        setState(result);
     }
 
     const postData = async () => {
@@ -77,30 +79,59 @@ export default function App() {
     }
 
 
-
-
     return (
-
         <div>
             <Tabs {...bindings} initialValue="1" align="center" leftSpace={0}>
                 <Tabs.Item label="Inicio" value="1" >
-
+                    Aqui va un texto con la explicación del experimento
                 </Tabs.Item>
                 <Tabs.Item label="Video 1" value="2">
-                    <video id="Conducir contra vía" ref={video} onPlay={detectVideoStarts} onEnded={detectVideoEnds}>
-                        <source src='/assets/CCV.mp4' type="video/mp4" />
-                    </video>
-                    <Button onClick={togglePlay}>Play Video</Button>
+                    <Display shadow caption={
+                        <Button onClick={togglePlay}>Play Video</Button>
+                    }>
+                        <video height="500rem" id="Conducir contra vía" ref={video} onPlay={detectVideoStarts} onEnded={detectVideoEnds}>
+                            <source src='/assets/CCV.mp4' type="video/mp4" />
+                        </video>
+                    </Display>
                 </Tabs.Item>
                 <Tabs.Item label="Video 2" value="3">
+                    <Display shadow caption={
+                        <Button onClick={togglePlay}>Play Video</Button>
+                    }>
+                        <video height="500rem" id="Pasar Luz Roja 1" ref={video} onPlay={detectVideoStarts} onEnded={detectVideoEnds}>
+                            <source src='/assets/PLR1.mp4' type="video/mp4" />
+                        </video>
+                    </Display>
                 </Tabs.Item>
                 <Tabs.Item label="Video 3" value="4">
+                    <Display shadow caption={
+                        <Button onClick={togglePlay}>Play Video</Button>
+                    }>
+                        <video height="500rem" id="Exceso de velocidad" ref={video} onPlay={detectVideoStarts} onEnded={detectVideoEnds}>
+                            <source src='/assets/EV.mp4' type="video/mp4" />
+                        </video>
+                    </Display>
                 </Tabs.Item>
                 <Tabs.Item label="Video 4" value="5">
+                    <Display shadow caption={
+                        <Button onClick={togglePlay}>Play Video</Button>
+                    }>
+                        <video height="500rem" id="Sin casco 1" ref={video} onPlay={detectVideoStarts} onEnded={detectVideoEnds}>
+                            <source src='/assets/SC1.mp4' type="video/mp4" />
+                        </video>
+                    </Display>
                 </Tabs.Item>
                 <Tabs.Item label="Video 5" value="6">
+                    <Display shadow caption={
+                        <Button onClick={togglePlay}>Play Video</Button>
+                    }>
+                        <video height="500rem" id="Pasar Luz Roja 2" ref={video} onPlay={detectVideoStarts} onEnded={detectVideoEnds}>
+                            <source src='/assets/PLR2.mp4' type="video/mp4" />
+                        </video>
+                    </Display>
                 </Tabs.Item>
                 <Tabs.Item label="Fin" value="7">
+                    Gracias por tu aporte
                 </Tabs.Item>
             </Tabs>
         </div>
